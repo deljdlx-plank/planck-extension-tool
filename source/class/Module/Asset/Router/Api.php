6fe2859b5d335726cf3f18e1a5e216e9c00df9b5
Jean luc <you@example.com>
2019-02-07 14:58:24 +0100

    synchro

--- source/class/Module/Asset/Router/Api.php
+++ source/class/Module/Asset/Router/Api.php
@@ -6,6 +6,7 @@ namespace Planck\Extension\Tool\Module\Asset\Router;
 
 use Planck\Extension\Tool\Module\Asset\Controller\LocalCSSFileSender;
 use Planck\Extension\Tool\Module\Asset\Controller\LocalJavascriptFileSender;
+use Planck\Helper\File;
 use Planck\Router;
 use Planck\ModuleResourceLoader;
 
@@ -19,10 +20,48 @@ class Api extends Router
     {
 
 
+        $this->get('tool-asset-api-get-extension-javascript', '`/tool/asset/api/get-extension-javascript`', function () {
+
+            $extensionName = $this->get('extension');
+            $extension = $this->application->getExtension($extensionName);
+
+            $javascript = $this->get('javascript');
+
+            $javascript = File::sanitizePath($javascript);
+
+            $file = realpath($extension->getAssetsFilepath().$javascript);
+            if(is_file($file)) {
+                echo file_get_contents($file);
+            }
+        })->contentType('text/javascript');
+
+
+        $this->get('tool-asset-api-get-extension-css', '`/tool/asset/api/get-extension-css`', function () {
+
+
+
+            $extensionName = $this->get('extension');
+            $extension = $this->application->getExtension($extensionName);
+
+
+
+
+            $css = $this->get('css');
+
+            $css = File::sanitizePath($css);
+
+            $file = realpath($extension->getAssetsFilepath().$css);
+            if(is_file($file)) {
+                echo file_get_contents($file);
+            }
+        })->contentType('text/css');
+
+
+
+
+
+
         $this->get('tool/get/javascript', '`/tool/api-get-javascript`', function () {
-
-
-
 
             $hash = $this->request->get('javascript');
             $controller = new LocalJavascriptFileSender($this->application);
@@ -30,6 +69,11 @@ class Api extends Router
 
 
         })->contentType('text/javascript');
+
+
+
+
+
 
 
         $this->get('tool/get/css', '`/tool/api-get-css`', function () {
